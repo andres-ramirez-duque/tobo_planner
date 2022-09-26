9ba914a235be1e21fd2ddfa22aeb770face5dcfb
AlanLindsay100 <alanlindsay100@gmail.com>
2022-09-26 18:07:55 +0100

    typo fix..

--- planning_service/state_builder.py
+++ planning_service/state_builder.py
@@ -68,7 +68,7 @@ class RosStateManagerProxy(StateManagerProxy):
     multi_var_items = []
     for source in (params, sensor_vals):
       if "multi_vars" in source:
-        multi_var_items += params["multi_vars"]
+        multi_var_items += source["multi_vars"]
     self.multi_vars = dict(multi_var_items)
     self.executed_action = params["last_executed_action"]
     self.previous_action = params["previous_action"]
