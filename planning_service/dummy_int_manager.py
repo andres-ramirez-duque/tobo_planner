f8f80e55cc7a8f905c254f668339c5320310caef
AlanLindsay100 <alanlindsay100@gmail.com>
2023-08-02 11:00:14 +0100

    dummy runner stopper

--- planning_service/dummy_int_manager.py
+++ planning_service/dummy_int_manager.py
@@ -1,5 +1,7 @@
 import int_manager as IM
 
+ADD_DUMMY_STOP_ON_ANY_KEY=False
+
 if __name__ == '__main__':
   #plan = ("doactivity2bold_intro_intronau_introstep_medium_low","progressprocstep1_introstep_preprocedure","goal")
   plan = """readytostart
@@ -9,6 +11,7 @@ ivstartpreprocedure
 pameducateonprocedure_ivdescription
 ppstartanxietymanagement
 ppamtestanxiety
+ppamengagementtest
 ppmakedivertionplan
 ppdivertor_song1
 ivcompletepreprocedure
@@ -25,4 +28,8 @@ ivdebrief_song1
 ivfinish_bye
 goal""".split()
   im = IM.int_manager(IM.dummy_ros_proxy(plan), "model0.9/state_frames_scenario.yaml")
+  if ADD_DUMMY_STOP_ON_ANY_KEY:
+    from pynput import keyboard
+    listener = keyboard.Listener(on_press=im.stop_message_event)
+    listener.start()  # start to listen on a separate thread
   im.init()
